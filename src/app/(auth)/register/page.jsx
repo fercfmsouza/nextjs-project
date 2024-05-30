0f28c8b81a9d709ea7dcb89f1a31e26a8df314fd
Fernanda Souza <fecafumee@gmail.com>
2024-05-30 11:23:54 +0200

    fix path

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,4 +1,4 @@
-import { RegisterForm } from '../../../components/registerForm/registerForm';
+import { RegisterForm } from '@/components/registerForm/RegisterForm';
 import styles from './register.module.css';
 
 const RegisterPage = () => {
