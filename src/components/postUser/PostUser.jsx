37a05adac7b3d71522c08bdf0ac5f61b8ff828a9
Fernanda Souza <fecafumee@gmail.com>
2024-05-30 14:38:27 +0200

    trying to solve compilation issue

--- src/components/postUser/PostUser.jsx
+++ src/components/postUser/PostUser.jsx
@@ -2,21 +2,7 @@ import Image from 'next/image';
 import { getUser } from '@/lib/data';
 import styles from './postUser.module.css';
 
-//FETCH DATA WITH AN API
-// const getData = async (userId) => {
-//   const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`, { cache: 'no-store' });
-
-//   if (!res.ok) {
-//     throw new Error('Something went wrogn');
-//   }
-
-//   return res.json();
-// };
-
 const PostUser = async ({ userId }) => {
-  //FETCH DATA WITH AN API
-  // const user = await getData(userId);
-
   //FETCH DATA WITHOUT AN API
   const user = await getUser(userId);
 
